21df361711ec84f97e71a1a7327d0b57d992280b
Enrique Piñero <enrpinfue@alum.us.es>
2020-06-03 19:59:04 +0200

    Arreglado bindeo al puerto

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/test",(req,res)=>{
 
 app.use("/",express.static(path.join(__dirname,"public")));
 
-app.listen(process.env.PORT | 8080,()=>{
+app.listen(process.env.PORT || 8080,()=>{
     console.log("Servidor listo");
 }).on("error",(e) =>{
     console.error("Ha ocurrido un problema");
